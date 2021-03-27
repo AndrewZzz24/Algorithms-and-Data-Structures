f6ecf7be14c393563b7a24c67f543a82379088c0
Andrew Z <70968410+AndrewZzz24@users.noreply.github.com>
2021-03-27 16:15:02 +0300

    Done

--- Lab 10/Task 2/main.cpp
+++ Lab 10/Task 2/main.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <iomanip>
 
 class Point {
 private:
+
     double x;
     double y;
+
 public:
     Point(double x, double y) : x(x), y(y) {}
 
     double getX() const {
+
         return x;
+
     }
 
     double getY() const {
+
         return y;
+
     }
 };
 
 void input(int &n, std::vector<Point> &vert) {
+
     std::cin >> n;
+
     for (size_t i = 0; i < n; i++) {
+
         double x, y;
         std::cin >> x >> y;
         vert.emplace_back(x, y);
+
     }
+
 }
 
 double get_distance(Point &p1, Point &p2) {
@@ -34,21 +46,24 @@ double get_distance(Point &p1, Point &p2) {
 }
 
 double find_min_mst(int n, std::vector<Point> &vert) {
+
     double result = 0;
-    int prev_vert_index = -1;
-    std::vector<int> key;
+    std::vector<double> key;
     key.assign(vert.size(), -1);
     key[0] = 0;
+
     while (true) {
 
-        int index, value = -1;
+        int index;
+        double value = -1;
+
         for (size_t i = 0, j = 1; i < key.size(); i++) {
 
-            if (j == 1 && key[i] != -1) {
+            if (j == 1 && key[i] > -1) {
                 j = 0;
                 index = i;
                 value = key[i];
-            } else if (key[i] != -1 && key[i] < value) {
+            } else if (key[i] > -1 && key[i] < value) {
                 index = i;
                 value = key[i];
             }
@@ -57,32 +72,40 @@ double find_min_mst(int n, std::vector<Point> &vert) {
 
         if (value == -1) break;
 
-        if (prev_vert_index != -1)
-            result += get_distance(vert[index], vert[prev_vert_index]);
+        if (key[index] > 0)
+            result += key[index];
 
         for (size_t i = 0; i < vert.size(); i++) {
 
-            if (i != index) {
+            if (i != index && key[i] > -2) {
                 double tmp = get_distance(vert[i], vert[index]);
-                if (key[i] > tmp || key[i]==-1)
+                if (key[i] > tmp || key[i] == -1)
                     key[i] = tmp;
             }
 
         }
-        prev_vert_index = index;
+
         key[index] = -2;
 
     }
+
     return result;
+
 }
 
 int main() {
-    freopen("C:\\Users\\Andrz\\CLionProjects\\Algorithms-and-Data-Structures\\Lab 10\\Task 2\\spantree.in", "r", stdin);
-    freopen("C:\\Users\\Andrz\\CLionProjects\\Algorithms-and-Data-Structures\\Lab 10\\Task 2\\spantree.out", "w",
-            stdout);
+
+    freopen("spantree.in", "r", stdin);
+    freopen("spantree.out", "w", stdout);
+
     int n;
     std::vector<Point> vertexes;
+
     input(n, vertexes);
-    std::cout << find_min_mst(n, vertexes) << std::endl;
+
+    std::cout << std::fixed;
+    std::cout << std::setprecision(3) << find_min_mst(n, vertexes) << std::endl;
+
     return 0;
+
 }
