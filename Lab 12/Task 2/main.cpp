800f1182d102a3fdbd8bd29a36d092fc34939396
Andrz <Andr.rostov24@gmail.com>
2021-04-25 19:54:42 +0300

    Done

--- Lab 12/Task 2/main.cpp
+++ Lab 12/Task 2/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 const int INF = 2e9;
 
@@ -14,47 +13,51 @@ int main() {
         std::cin >> a[i];
 
     std::vector<int> d(n + 1, INF);
-    std::vector<int> parent(n, -1);
+    std::vector<int> parent(n + 1, -1);
+    std::vector<int> pos(n + 1, -1);
 
-    int maximum = -INF, res_num = 0, index_start = -2;
+    int lenght = 0;
+    int index_start;
+
+    d[0] = -INF;
+
     for (int i = 0; i < n; i++) {
 
         int index = i;
-        int left = -1, right = n - 1, middle;
+        int left = 0, right = n, middle;
 
         while (right - left != 1) {
 
             middle = (right + left) / 2;
 
-            if (d[middle] != INF && a[index] > a[d[middle]])
+            if (a[index] > d[middle])
                 left = middle;
             else
                 right = middle;
         }
 
-        d[right] = index;
+        if (d[right - 1] < a[index] && a[index] < d[right]) {
 
-        if (right != 0)
-            parent[index] = d[right - 1];
-
-        if (d[right + 1] == INF && a[d[right]] >= maximum) {
-
-            maximum = a[d[right]];
-            index_start = d[right];
-            res_num = right + 1;
+            d[right] = a[index];
+            pos[right] = index;
+            parent[index] = pos[right - 1];
+            lenght = std::max(lenght, right);
 
         }
     }
 
-    std::cout << res_num << std::endl;
     std::vector<int> result;
 
-    while (index_start != -1 && res_num != 0) {
+    index_start = pos[lenght];
+
+    while (index_start != -1) {
 
         result.push_back(a[index_start]);
         index_start = parent[index_start];
 
     }
+
+    std::cout << result.size() << std::endl;
 
     for (int i = result.size() - 1; i >= 0; i--)
         std::cout << result[i] << ' ';
