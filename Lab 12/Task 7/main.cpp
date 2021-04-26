b7deeca8161496458ab47b4be9604f3421226fcb
Andrz <Andr.rostov24@gmail.com>
2021-04-26 14:48:11 +0300

    Done

--- Lab 12/Task 7/main.cpp
+++ Lab 12/Task 7/main.cpp
@@ -2,39 +2,29 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<std::vector<int>> maxtrix;
-std::vector<int> a;
-
-void findans(int i, int j) {
-    if (maxtrix[i][j] == 0)
-        return;
-    if (maxtrix[i - 1][j] == maxtrix[i][j])
-        findans(i - 1, j);
-    else {
-        findans(i - 1, j - a[i]);
-        std::cout << i << ' ';
-    }
-}
-
 int main() {
 
-    //freopen("knapsack.in", "r", stdin);
-    //freopen("knapsack.out", "w", stdout);
+    freopen("knapsack.in", "r", stdin);
+    freopen("knapsack.out", "w", stdout);
+
+    std::vector<std::vector<int>> maxtrix;
+    std::vector<int> a;
 
     int s, n;
 
     std::cin >> s >> n;
-    a.resize(n);
 
-    for (int i = 0; i < n; i++)
+    a.resize(n + 1);
+
+    for (int i = 1; i <= n; i++)
         std::cin >> a[i];
 
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n; i++) {
 
         maxtrix.push_back({0});
 
-        for (int j = 0; j < s; j++)
+        for (int j = 0; j <= s; j++)
 
             maxtrix[i].push_back(0);
 
@@ -45,7 +35,7 @@ int main() {
         for (int j = 1; j <= s; j++) {
 
             if (j >= a[i])
-                maxtrix[i][j] = std::max(maxtrix[i - 1][j], maxtrix[i - 1][j - a[i]]);
+                maxtrix[i][j] = std::max(maxtrix[i - 1][j], maxtrix[i - 1][j - a[i]] + a[i]);
 
             else
                 maxtrix[i][j] = maxtrix[i - 1][j];
@@ -53,7 +43,15 @@ int main() {
         }
 
     }
-    findans(n - 1, s - 1);
 
+//    for (int i = 0; i <= n; i++) {
+//        for (int j = 0; j < s; j++)
+//            std::cout << maxtrix[i][j] << ' ';
+//        std::cout << std::endl;
+//    }
+
+    std::cout << maxtrix[n][s] << std::endl;
+
+    return 0;
 
 }
