dacf43e3414626d59d292e035735399dc2c7e9c4
AndrewZzz24 <Andr.rostov24@gmail.com>
2021-04-26 20:28:51 +0300

    Done

--- Lab 12/Task 8/main.cpp
+++ Lab 12/Task 8/main.cpp
@@ -1,5 +1,49 @@
 #include <iostream>
+#include <vector>
 
-int main(){
+int main() {
+
+
+    std::vector<std::vector<int>> d;
+    std::vector<int> a;
+
+    int n, infinity = 1000000000;
+
+    std::cin >> n;
+    a.resize(n);
+
+    for (int i = 0; i < n; i++)
+        std::cin >> a[i];
+
+    for (int i = 0; i < n; i++) {
+
+        d.push_back({0});
+
+        for (int j = 0; j < n; j++) {
+
+            d[i].push_back(0);
+            if (i == j)
+                d[i][j] = 1;
+
+        }
+
+    }
+
+    for (int i = n - 2; i > -1; i--) {
+
+        for (int j = i + 1; j < n; j++) {
+
+            if (a[i] != a[j])
+                d[i][j] = (d[i][j - 1] + d[i + 1][j] - d[i + 1][j - 1]) % infinity;
+            else
+                d[i][j] = (d[i + 1][j] + d[i][j - 1] + 1) % infinity;
+
+        }
+
+    }
+
+    std::cout << (d[0][n - 1] + infinity) % infinity << std::endl;
+
     return 0;
+
 }
